0942a64f70e2a2aa651857cf72a995bb0fb369c9
shihaipeng03 <32051279+shihaipeng03@users.noreply.github.com>
2019-08-09 00:23:10 +0800

    19.6.4

--- 程序/telegraph/telegraph.ino
+++ 程序/telegraph/telegraph.ino
@@ -19,13 +19,13 @@
 #define DOT_UP_DELAY 120    //打点等待时间  
 #define DOT_DOWN_DELAY 50   //打点等待时间   //要符合舵机动作幅度的时间，延时过小会导致无法打到
 
-#define DOT_UP 1700     //打点舵机提升到的位置，让笔刚好贴到纸上，不能抬的幅度过高
-#define DOT_DOWN 1900   //下降后的位置
+#define DOT_UP 1100     //打点舵机提升到的位置，让笔刚好贴到纸上，不能抬的幅度过高
+#define DOT_DOWN 1200   //下降后的位置
 //    
-#define SERVO_MIN 110   //写字舵机的字顶部位置
-#define SERVO_MAX 140   //底部位置    
+#define SERVO_MIN 25   //写字舵机的字顶部位置
+#define SERVO_MAX 65   //底部位置    
 
-#define SERVO_STEPS 15  //字符的高度，数值越小，字符越大，建议范围（10~30）
+#define SERVO_STEPS 16  //字符的高度，数值越小，字符越大，建议范围（10~30）
 #define LINE_TAB 9     //字符的基线位置，过大会超出纸的高度
 
 #define SERVO_STEP (SERVO_MAX - SERVO_MIN) / SERVO_STEPS
@@ -48,10 +48,11 @@ void setup() {
   servo_dot.write(DOT_DOWN);
  
   servo_arm.attach(SERVO_PIN);
+  servo_arm.write((SERVO_MIN + SERVO_MAX) / 2);
   
   pinMode(DOT_PIN, OUTPUT);
  
-  printString(" HELLO WORLD! HAPPY TIME    ");   //测试写一些内容
+  printString("Test OK!");   //测试写一些内容
 }
 
 
@@ -136,6 +137,3 @@ void loop()
   Bluetooth.write(1);
   delay(1500);
 }
-
-
-
